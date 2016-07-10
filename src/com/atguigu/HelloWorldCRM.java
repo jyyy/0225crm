0ba6be83240a9602cfb7a30b0a90b248b4d057f2
jyyy <920707671@qq.com>
2016-07-10 21:03:31 +0800

    111
    111

--- src/com/atguigu/HelloWorldCRM.java
+++ src/com/atguigu/HelloWorldCRM.java
@@ -4,6 +4,7 @@ public class HelloWorldCRM {
 	public static void main(String[] args) {
 		System.out.println("hello !");
 		System.out.println("hello 2!");
+		System.out.println("hello git 1!");
 	}
 
 }
